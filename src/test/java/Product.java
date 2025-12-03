61eb4892d476480cc2229fad17f67f8d853140a7
Wiku Yoga Ndaru <wikundaru3@gmail.com>
2025-12-03 22:13:36 +0700

    validating data to the databases

--- src/test/java/Product.java
+++ src/test/java/Product.java
@@ -1,24 +1,34 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.sql.*;
+
 import static org.hamcrest.Matchers.*;
 
 
 public class Product {
 
+    public final String URL = "https://api-staging.lp3es-prisma.co.id/v1";
     public String title, description, imageUrl;
     public int productId, count;
     public float rate;
 
     @Test
     public void getProductList() {
-        RestAssured.baseURI = "https://fakestoreapi.com";
+        RestAssured.baseURI = URL;
         Response response = RestAssured
                 .given()
+                .header("device_type", "WEB")
+                .header("Language", "id")
+                .header("version_code", "1")
+                .header("version_name", "1.0.0")
                 .when()
-                .get("/products");
+                .get("/product");
 
 
+        /*
         response.then()
                 .body("size()", equalTo(20))
                 .assertThat().statusCode(200);
@@ -26,25 +36,79 @@ public class Product {
         title = response
                 .jsonPath().getString("[0]");
 
+         */
+
+        title = response
+                .jsonPath().getString("data[0].product_id");
+        response.then()
+                        .statusCode(200);
+
 
         System.out.println(title);
     }
 
 
     @Test
-    public void getSpecifictProduct(){
-        RestAssured.baseURI = "https://fakestoreapi.com";
+    public void getSpecifictProduct() throws SQLException {
+        Connection connectDatabase;
+        Statement statement;
+        RestAssured.baseURI = URL;
         Response response = RestAssured
                 .given()
+                .header("device_type", "WEB")
+                .header("Language", "id")
+                .header("version_code", "1")
+                .header("version_name", "1.0.0")
                 .pathParam("id", 3)
                 .when()
-                .get("/products/{id}");
+                .get("/product/{id}");
 
         System.out.println(response.print());
 
+
+        //validating the status code and response data
         response.then()
-                .body("id", equalTo(3))
+                .body("data.product_id", equalTo(3))
                 .statusCode(200);
+
+        //validating to the database but still using mock database
+        connectDatabase = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/db_bussid",
+                "root",
+                ""
+        );
+        statement = connectDatabase.createStatement();
+        String query = "select * from `mod` p where p.id  = 1";
+        ResultSet set = statement.executeQuery(query);
+
+        Assert.assertTrue(set.next(), "Data tidak ditemukan!");
+
+        Assert.assertEquals(set.getInt("id"), 1);
+
+        String name = set.getString("name");
+        System.out.println(name);
     }
+
+    @Test
+    public void getSpesifctProductNotFound(){
+        RestAssured.baseURI = URL;
+        Response response = RestAssured
+                .given()
+                .header("device_type", "WEB")
+                .header("Language", "id")
+                .header("version_code", "1")
+                .header("version_name", "1.0.0")
+                .pathParam("id", 999)
+                .when()
+                .get("/product/{id}");
+
+        System.out.println(response.print());
+        response.then()
+                .statusCode(404)
+                .body("meta.msg", equalTo("Not Found"));
+    }
+
+
+
 
 }
